2016938fa840beefb873c736403839d3302750fd
Venkat Sai Jarugula <jarugulavenkat7@gmail.com>
2022-04-19 22:42:47 -0500

    Added static class Job1Finalizer,constructors for RankedPage and VotingPage

---
+++ VenkatSaiJarugula/src/main/java/edu/nwmissouri/sec02group03/jarugula/VotingPage.java
@@ -0,0 +1,9 @@
+package edu.nwmissouri.sec02group03.jarugula;
+
+public class VotingPage {
+
+	public VotingPage(String voterName, Integer contributorVotes) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
