85509b173e80a75f078e0f6a8df12fb955af878c
madhukonda <madhukonda63@gmail.com>
2022-04-19 22:12:18 -0500

    added files

---
+++ MadhuBabuKonda/src/main/java/edu/nwmsu/scetion02group03/madhubabu/RankedPage.java
@@ -0,0 +1,12 @@
+package edu.nwmsu.scetion02group03.madhubabu;
+
+import java.util.ArrayList;
+
+public class RankedPage {
+    public RankedPage(){
+
+    }
+    public RankedPage(String s, ArrayList<VotingPage> v){
+        
+    }
+}
