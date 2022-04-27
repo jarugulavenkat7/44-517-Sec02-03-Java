76ceaa071514ac7c51ecb10dbc70500419829826
madhukonda <madhukonda63@gmail.com>
2022-04-27 12:03:23 -0500

    Added Files

--- MadhuBabuKonda/src/main/java/edu/nwmsu/scetion02group03/madhubabu/RankedPage.java
+++ MadhuBabuKonda/src/main/java/edu/nwmsu/scetion02group03/madhubabu/RankedPage.java
@@ -1,12 +1,46 @@
 package edu.nwmsu.scetion02group03.madhubabu;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RankedPage {
-    public RankedPage(){
-
+public class RankedPage implements Serializable{
+    String voter;
+    double rank;
+    ArrayList<VotingPage> voterList = new ArrayList<>();
+    
+    public RankedPage(String voter,double rank, ArrayList<VotingPage> voters){
+        this.voter = voter;
+        this.voterList = voters;
+        this.rank = rank;
+    }    
+    public RankedPage(String voter, ArrayList<VotingPage> voters){
+        this.voter = voter;
+        this.voterList = voters;
+        this.rank = 1.0;
+    }    
+    // retrun voter
+    public String getVoter() {
+        return voter;
     }
-    public RankedPage(String s, ArrayList<VotingPage> v){
-        
+    // set voter
+    public void setVoter(String voter) {
+        this.voter = voter;
+    }
+    //  return voter list
+    public ArrayList<VotingPage> getVoterList() {
+        return voterList;
+    }
+    // set voter list
+    public void setVoterList(ArrayList<VotingPage> voterList) {
+        this.voterList = voterList;
+    }
+    // to string
+    @Override
+    public String toString(){
+        return this.voter +"<"+ this.rank +","+ voterList +">";
+    }
+    // return rank
+    public double getRank() {
+        return this.rank;
     }
 }
