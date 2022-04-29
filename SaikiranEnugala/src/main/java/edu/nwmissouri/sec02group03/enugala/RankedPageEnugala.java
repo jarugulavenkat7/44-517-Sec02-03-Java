4c18a94b2432706500b1123214432a6a8e2953d5
saikiranreddyenugala <77818423+saikiranreddyenugala@users.noreply.github.com>
2022-04-29 15:56:49 -0500

    updated ranked page

--- SaikiranEnugala/src/main/java/edu/nwmissouri/sec02group03/enugala/RankedPageEnugala.java
+++ SaikiranEnugala/src/main/java/edu/nwmissouri/sec02group03/enugala/RankedPageEnugala.java
@@ -3,16 +3,43 @@ package edu.nwmissouri.sec02group03.enugala;
 import java.util.ArrayList;
 
 public class RankedPageEnugala {
-String key;
-ArrayList<VotingPageEnugala> voters;
-    public RankedPageEnugala(String voter, ArrayList<VotingPageEnugala> voters){
-        this.key = voter;
-        this.voters = voters;
-    }
-    @Override
-    public String toString(){
-        return key + voters;
-    }
-	
+    String voter;
+    double rank;
+    ArrayList<VotingPageEnugala> voterList = new ArrayList<>();
 
+    public RankedPageEnugala(String voter, double rank, ArrayList<VotingPageEnugala> voters) {
+        this.voter = voter;
+        this.voterList = voters;
+        this.rank = rank;
+
+    }
+
+    public RankedPageEnugala(String voter, ArrayList<VotingPageEnugala> voterList) {
+        this.voter = voter;
+        this.voterList = voterList;
+    }
+
+    public String getVoter() {
+        return voter;
+    }
+
+    public void setVoter(String voter) {
+        this.voter = voter;
+    }
+
+    public double getRank() {
+        return rank;
+    }
+
+    public void setRank(double rank) {
+        this.rank = rank;
+    }
+
+    public ArrayList<VotingPageEnugala> getVoterList() {
+        return voterList;
+    }
+
+    public void setVoterList(ArrayList<VotingPageEnugala> voterList) {
+        this.voterList = voterList;
+    }
 }
