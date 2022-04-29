5ca3f6d49e2fc1853d66a06f7626779e8a5cc15c
Venkat Sai Jarugula <jarugulavenkat7@gmail.com>
2022-04-29 17:27:10 -0500

    updated MinimalPageRank.java

--- VenkatSaiJarugula/src/main/java/edu/nwmissouri/sec02group03/jarugula/MinimalPageRankJarugula.java
+++ VenkatSaiJarugula/src/main/java/edu/nwmissouri/sec02group03/jarugula/MinimalPageRankJarugula.java
@@ -1,13 +1,16 @@
 package edu.nwmissouri.sec02group03.jarugula;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Comparator;
 
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
+import org.apache.beam.sdk.transforms.Combine;
 import org.apache.beam.sdk.transforms.Count;
 import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Filter;
@@ -15,6 +18,8 @@ import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.Flatten;
 import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.Max;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.PCollection;
 import org.apache.beam.sdk.values.PCollectionList;
@@ -24,23 +29,6 @@ import org.apache.beam.sdk.values.TypeDescriptors;
 public class MinimalPageRankJarugula {
 
 
-   // DEFINE DOFNS
-  // ==================================================================
-  // You can make your pipeline assembly code less verbose by defining
-  // your DoFns statically out-of-line.
-  // Each DoFn<InputT, OutputT> takes previous output
-  // as input of type InputT
-  // and transforms it to OutputT.
-  // We pass this DoFn to a ParDo in our pipeline.
-
-  /**
-   * DoFn Job1Finalizer takes KV(String, String List of outlinks) and transforms
-   * the value into our custom RankedPage Value holding the page's rank and list
-   * of voters.
-   * 
-   * The output of the Job1 Finalizer creates the initial input into our
-   * iterative Job 2.
-   */
   static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
     
     @ProcessElement
@@ -59,7 +47,75 @@ public class MinimalPageRankJarugula {
       receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
     }
   }
+  
+  static class Job2Mapper extends DoFn<KV<String, RankedPage>, KV<String, RankedPage>> {
+	    @ProcessElement
+	    public void processElement(@Element KV<String, RankedPage> element,
+	        OutputReceiver<KV<String, RankedPage>> receiver) {
+	      Integer votes = 0;
+	      ArrayList<VotingPage> voters = element.getValue().getPagesVoted();
+	      if (voters instanceof Collection) {
+	        votes = ((Collection<VotingPage>) voters).size();
+	      }
 
+	      for (VotingPage vp : voters) {
+	        String pageName = vp.getName();
+	        Double pageRank = vp.getRank();
+	        String contributingPageName = element.getKey();
+	        Double contributingPageRank = element.getValue().getRankValue();
+	        VotingPage contributor = new VotingPage(contributingPageName, contributingPageRank, votes);
+	        ArrayList<VotingPage> arr = new ArrayList<>();
+	        arr.add(contributor);
+	        receiver.output(KV.of(vp.getName(), new RankedPage(pageName, pageRank, arr)));
+	      }
+	    }
+	  }
+  
+  
+  static class Job2Updater extends DoFn<KV<String, Iterable<RankedPage>>, KV<String, RankedPage>> {
+	    @ProcessElement
+	    public void processElement(@Element KV<String, Iterable<RankedPage>> element,
+	        OutputReceiver<KV<String, RankedPage>> receiver) {
+	      String thisPage = element.getKey();
+	      Iterable<RankedPage> rankedPages = element.getValue();
+	      Double dampingFactor = 0.85;
+	      Double updatedRank = (1 - dampingFactor);
+	      ArrayList<VotingPage> newVoters = new ArrayList<VotingPage>();
+
+	      for (RankedPage pg : rankedPages) {
+	        if (pg != null) {
+	          for (VotingPage vp : pg.getPagesVoted()) {
+	            newVoters.add(vp);
+	            updatedRank += (dampingFactor) * vp.getRank() / (double) vp.getVotes();
+	          }
+	        }
+	      }
+
+	      receiver.output(KV.of(thisPage, new RankedPage(thisPage, updatedRank, newVoters)));
+	    }
+	  }
+// I wrote this method with the help and Pramod and Sai Kiran Gangidi  
+  static class Job3 extends DoFn<KV<String, RankedPage>, KV<String, Double>> {
+	    @ProcessElement
+	    public void processElement(@Element KV<String, RankedPage> element,
+	        OutputReceiver<KV<String, Double>> receiver) {
+	      String currentPage = element.getKey();
+	      Double currentPageRank = element.getValue().getRankValue();
+
+	      receiver.output(KV.of(currentPage, currentPageRank));
+	    }
+	  }
+ // I wrote this method with the help and Pramod Gonegari and Sai Kiran Gangidi  
+  public static class Job3Final implements Comparator<KV<String, Double>>, Serializable {
+	    @Override
+	    public int compare(KV<String, Double> value1, KV<String, Double> value2) {
+	      return value1.getValue().compareTo(value2.getValue());
+	    }
+	  }
+  
+
+
+ // I wrote this method with the help and Pramod Gonegari and Sai Kiran Gangidi  
   public static void main(String[] args) {
 
     PipelineOptions options = PipelineOptionsFactory.create();
@@ -76,14 +132,43 @@ public class MinimalPageRankJarugula {
     PCollection<KV<String, String>> mergedList = pCollectionList.apply(Flatten.<KV<String, String>>pCollections());
     PCollection<KV<String, Iterable<String>>> urlToDocs = mergedList.apply(GroupByKey.<String, String>create());
 
-    PCollection<String> pLinksStr = urlToDocs.apply(
-        MapElements.into(
-            TypeDescriptors.strings())
-            .via((mergeOut) -> mergeOut.toString()));
+    PCollection<KV<String, RankedPage>> jobTwoin = urlToDocs.apply(ParDo.of(new Job1Finalizer()));
 
-    pLinksStr.apply(TextIO.write().to("jarugulaOut"));
+    PCollection<KV<String, RankedPage>> jobTwoout = null;
+
+    PCollection<KV<String, RankedPage>> mappedKVs = null;
+
+    PCollection<KV<String, Iterable<RankedPage>>> reducedKVs = null;
+    int iterations = 50;
+    for (int i = 1; i <= iterations; i++) {
+      mappedKVs = jobTwoin.apply(ParDo.of(new Job2Mapper()));
+
+      reducedKVs = mappedKVs
+          .apply(GroupByKey.<String, RankedPage>create());
+
+      jobTwoout = reducedKVs.apply(ParDo.of(new Job2Updater()));
+
+      jobTwoin = jobTwoout;
+
+    }
+    PCollection<KV<String, Double>> maximumRank = jobTwoout.apply(ParDo.of(new Job3()));
+
+    PCollection<KV<String, Double>> finalMaximum = maximumRank.apply(Combine.globally(Max.of(new Job3Final())));
+
+    PCollection<String> fnl = finalMaximum.apply(MapElements.into(
+        TypeDescriptors.strings())
+        .via(kv -> kv.toString()));
+    fnl.apply(TextIO.write().to("jarugulaOut"));
 
     p.run().waitUntilFinish();
+    // PCollection<String> pLinksStr = urlToDocs.apply(
+    //     MapElements.into(
+    //         TypeDescriptors.strings())
+    //         .via((mergeOut) -> mergeOut.toString()));
+
+    // pLinksStr.apply(TextIO.write().to("jarugulaOut"));
+
+    // p.run().waitUntilFinish();
   }
 
   private static PCollection<KV<String, String>> jarugulaMapper01(Pipeline p, String dataFolder, String dataFile) {
